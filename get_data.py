13bb3eeebd74ba2e47c5bced4bfd39719a0902ea
trucndm <trucndmse172007@fpt.edu.vn>
2024-06-11 09:56:03 +0700

    Add things

---
+++ get_data.py
@@ -0,0 +1,228 @@
+from datetime import datetime, timedelta, timezone
+import pandas as pd
+import numpy as np
+import requests
+import os
+import optuna
+import logging
+import sys
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from datetime import datetime, timedelta
+from ta import add_all_ta_features
+
+"""## Variables"""
+
+current_storage = os.path.join("d:", "data")
+vn30f_storage = os.path.join(current_storage, 'vn30f')
+
+start_time = 0
+now_time = 9999999999
+symbol = 'VN30F1M'
+rolling_window = 1 # Số phút muốn dự đoán tiếp theo
+
+# Add stream handler of stdout to show the messages
+optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
+study_name = "btc1-study"  # Unique identifier of the study.
+storage_name = "sqlite:///{}.db".format(study_name)
+
+"""# Function for data"""
+
+def scale_data(data):
+    scaler = StandardScaler()
+    scaler.fit(data)
+    data=pd.DataFrame(scaler.transform(data), index=data.index, columns=data.columns)
+
+    return data
+
+def split_data(data):
+    """
+    Hàm này chia dữ liệu thành 2 phần: tập huấn luyện và tập hold out.
+
+    Args:
+    data (pandas.DataFrame): DataFrame chứa dữ liệu cần chia.
+
+    Returns:
+    pandas.DataFrame: DataFrame chứa dữ liệu tập huấn luyện.
+    pandas.DataFrame: DataFrame chứa dữ liệu tập giữ lại.
+    """
+    # Chia dữ liệu thành 3 phần
+    new_part = np.array_split(data, 3)
+
+    # Access each part individually
+    hold_out = new_part[2]
+    train_data = pd.concat([new_part[0], new_part[1]], axis=0)
+
+    return train_data, hold_out
+
+def split_optuna_data(data):
+    """
+    Hàm này chia dữ liệu thành các tập train và test để sử dụng trong quá trình tối ưu hóa bằng Optuna.
+
+    Args:
+    data (pandas.DataFrame): DataFrame chứa dữ liệu cần chia.
+
+    Returns:
+    pandas.DataFrame: DataFrame chứa dữ liệu train (đã được chuẩn hóa).
+    pandas.DataFrame: DataFrame chứa dữ liệu test (đã được chuẩn hóa).
+    pandas.Series: Series chứa nhãn tương ứng với dữ liệu train.
+    pandas.Series: Series chứa nhãn tương ứng với dữ liệu test.
+    """
+    # Chia dữ liệu thành tập train và tập hold out
+    train_data, _ = split_data(data)
+
+    # Loại bỏ các cột không cần thiết
+    if 'Open' in train_data.columns:
+        optuna_data = train_data.drop(['Open','High','Low','Close','Volume', 'Return', 'Date', 'time'], axis=1)
+    else:
+        optuna_data = train_data.drop(['Close', 'Return', 'Date', 'time'], axis=1)
+
+    # Chuẩn hóa dữ liệu
+    optuna_data = scale_data(optuna_data)
+
+    # Chia dữ liệu thành tập train và tập test
+    X_train, X_valid, y_train, y_valid = train_test_split(optuna_data, train_data['Return'], test_size=0.5, shuffle=False)
+
+    return X_train, X_valid, y_train, y_valid, train_data
+
+"""## Get data"""
+
+def get_vn30f(start_time, now_time, symbol):
+    def vn30f():
+            return requests.get(f"https://services.entrade.com.vn/chart-api/chart?from={start_time}&resolution=1&symbol={symbol}&to={now_time}").json()
+    vn30fm = pd.DataFrame(vn30f()).iloc[:,:6]
+    vn30fm['t'] = vn30fm['t'].astype(int).apply(lambda x: datetime.utcfromtimestamp(x) + timedelta(hours = 7))
+    vn30fm.columns = ['Date','Open','High','Low','Close','Volume']
+    ohlc_dict = {
+        'Open': 'first',
+        'High': 'max',
+        'Low': 'min',
+        'Close': 'last',
+        'Volume': 'sum',}
+    vn30fm = pd.DataFrame(vn30f()).iloc[:,:6]
+    vn30fm['t'] = vn30fm['t'].astype(int).apply(lambda x: datetime.utcfromtimestamp(x) + timedelta(hours = 7))
+    vn30fm.columns = ['Date','Open','High','Low','Close','Volume']
+    dt_object = datetime.utcfromtimestamp(start_time) + timedelta(hours = 7)
+    now_object = datetime.utcfromtimestamp(now_time) + timedelta(hours = 7)
+
+    print(f'===> Data {symbol} from {dt_object} to {now_object} has been appended ')
+
+    return vn30fm
+
+df = get_vn30f(start_time, now_time, symbol)
+
+data = df.copy()
+
+"""## Preprocess"""
+
+from scipy.stats.mstats import winsorize
+# Áp dụng Winsorization
+#data['Close'] = winsorize(data['Close'], limits=[0.05, 0.05])
+
+def process_data(data):
+    data.set_index('Date', inplace =True)
+    data.columns = ['Open','High','Low','Close','Volume']
+
+    data['Date'] = [str(i)[:10] for i in data.index]
+    data['time'] = [str(i)[11:] for i in data.index]
+
+    data = data[~data.index.duplicated(keep='first')] # Handling duplicate
+    data_model = data.pivot(index = 'Date', columns = 'time', values = ['Open','High','Low','Close','Volume']).ffill(axis = 1).stack().reset_index() # Handling missing values
+    # data_model.columns = ['Date','time','Close']
+
+    return data_model
+
+data = process_data(data)
+
+data
+
+"""## Create features"""
+
+def generate_features(df, shift=1):
+    """
+    Hàm này tạo ra các features mới từ dữ liệu cổ phiếu.
+
+    Args:
+    df (pandas.DataFrame): DataFrame chứa dữ liệu cần tạo features, có các cột 'open', 'high', 'low', 'close' và 'volume'.
+
+    Returns:
+    pandas.DataFrame: DataFrame đã được mở rộng với các features mới.
+    """
+
+    # Thêm tất cả các đặc trưng kỹ thuật từ thư viện TA-Lib
+    df_shift = df.shift(shift)
+    df_shift = add_all_ta_features(df_shift, open="Open", high="High", low="Low", close="Close", volume="Volume")
+
+    # Drop the original OHLCV columns from the shifted DataFrame
+    df_shift = df_shift.drop(columns=['Date','time', 'Open','High','Low','Close','Volume'])
+
+    df = pd.concat([df, df_shift], axis=1)
+
+    ### Ví dụ các features thêm tay
+    # Tính phần trăm thay đổi của giá đóng cửa trong 5 phút
+    df['Trend_5min'] = df['Close'].shift(shift).pct_change(periods=5)
+
+    # Tính độ lệch chuẩn của giá đóng cửa trong 30 phút
+    df['Std_Rolling_30min'] = df['Close'].shift(shift).rolling(window=30, min_periods=1).std()
+
+    # Tính độ lệch của giá đóng cửa so với trung bình động 30 phút
+    ma_30min = df['Close'].shift(shift).rolling(window=30, min_periods=1).mean()
+    df['Close_Minus_MA_30min'] = df['Close'].shift(shift) - ma_30min
+
+    # Tính trung bình động 10 phút của giá đóng cửa
+    df['SMA_10'] = df['Close'].shift(shift).rolling(window=10, min_periods=1).mean()
+
+    # Identify numeric columns
+    numeric_cols = df.select_dtypes(include=[np.number]).columns
+
+    # Replace infinite values with NaN in numeric columns
+    df[numeric_cols] = df[numeric_cols].replace([np.inf, -np.inf], np.nan)
+
+    # Điền các giá trị NaN với 0
+    df = df.fillna(0)
+
+    return df
+
+data  = generate_features(data)
+
+"""Thêm các feature khác, ví dụ giá vàng (xem xét phù hợp với thời gian trong data)
+
+# Add Predict Features
+"""
+
+data['Return'] = (data['Close'].shift(-rolling_window) - data['Close'])/data['Close']
+data = data.fillna(0)
+data = data.drop(index=0)
+
+"""# Feature Engineering"""
+from sklearn.linear_model import *
+def drop_high_corr_columns(df, threshold=0.6):
+    ohlcv_columns = {'Date','time', 'Open','High','Low','Close','Volume'}
+
+     # Identify non-OHLCV and non-date/time columns
+    non_corr_columns = [col for col in df.columns if col not in ohlcv_columns]
+
+    # Compute the correlation matrix only for non-OHLCV and non-date/time columns
+    corr_matrix = df[non_corr_columns].corr().abs()
+
+    # Create a set to keep track of columns to drop
+    to_drop = set()
+
+    # Iterate over the upper triangle of the correlation matrix
+    for i in range(len(corr_matrix.columns)):
+        for j in range(i + 1, len(corr_matrix.columns)):
+            if corr_matrix.iloc[i, j] > threshold:
+                # If the correlation is higher than the threshold, mark the column with the higher index to drop
+                col_to_drop = corr_matrix.columns[j]
+                if col_to_drop not in ohlcv_columns:
+                    to_drop.add(col_to_drop)
+
+    # Drop the columns from the DataFrame
+    df_dropped = df.drop(columns=to_drop)
+    return df_dropped
+
+data = drop_high_corr_columns(data)
+
+data.to_csv('save_data.csv')
+
